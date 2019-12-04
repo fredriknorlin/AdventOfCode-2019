f2d4b335fcdfa18cd6967578d9193e4c0b9a2e82
Fredrik <fredrik.norlin@hotmail.se>
2019-12-04 10:48:22 +0100

    Add solution for day04

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# AdventOfCode-2019
+# Advent Of Code 2019
+
+Advent calendar of small programming puzzles:
+https://adventofcode.com/
+
+Will try to solve all puzzles using Python
---
+++ day04/day04b.py
@@ -0,0 +1,10 @@
+import re
+
+with open('input04.txt') as file:
+    bounds = list(map(int, file.read().split('-')))
+    
+    count = 0
+    for i in range(bounds[0], bounds[1]):
+        if all(re.search(regex, str(i)) for regex in [r'^\d{6}$', r'^(\d)*(?!\1)(\d)\2{1}(?!\2)', r'^0*1*2*3*4*5*6*7*8*9*$']):
+            count += 1
+    print(count)
